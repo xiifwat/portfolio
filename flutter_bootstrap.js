51de13d0bafbf4dfeed91cef86d4ad8353dcc398
Tawfiqur Rahman <tawfiqur@brainstation-23.com>
2025-06-28 11:25:53 +0600

    Updated favicon and others

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1953084555"
+    serviceWorkerVersion: "3219515524"
   }
 });
